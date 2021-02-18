9289058a5ae6fb37a97e52fca64a6b03247dbaec
HesenNaghiyev <45911408+HesenNaghiyev@users.noreply.github.com>
2021-02-19 01:22:18 +0400

    Update rollbackInterests.sql

--- rollbackInterests.sql
+++ rollbackInterests.sql
@@ -1,5 +1,5 @@
 begin;
-alter table interests add column interest varchar(15);
+alter table interests add column interest text;
 insert into interests(interest,student_id) select u.* from interests, unnest(interests) with ordinality as u;
 delete from interests where interest is null;
 alter table interests drop column interests;
