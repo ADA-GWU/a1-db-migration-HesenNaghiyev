88b17b59b78f8903828dc745ad85b789bee12a8d
HesenNaghiyev <45911408+HesenNaghiyev@users.noreply.github.com>
2021-02-19 01:22:55 +0400

    Update interestsMigrate.sql

--- interestsMigrate.sql
+++ interestsMigrate.sql
@@ -1,5 +1,5 @@
 begin;
-alter table interests add column INTERESTS varchar(15) [];
+alter table interests add column INTERESTS text [];
 select array_agg(interest) from interests group by(student_id);
 insert into interests(student_id, interests) select student_id, array_agg(interest) from interests group by(student_id);
 delete from  interests where interests is  null;
